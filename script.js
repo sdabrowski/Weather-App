1d6db9685e47191916cf2e576a46059e519bdbde
sdabrowski <sdabrow2@gmail.com>
2017-05-14 23:52:14 -0500

    Updated and nearly complete weather app

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function getWeather(getZip) {
     $.getJSON(urlf, function(jd) { 
         $('#result').html(jd); 
         resultForecast = jd;
+
+        updateTextFore(resultForecast);
     });
 
     var urlc = "http://api.wunderground.com/api/fd1358c429691325/conditions/q/" + getZip + ".json";
@@ -27,11 +29,43 @@ function getWeather(getZip) {
         resultConditions = jd;
 
         //For some reason, the variables assigned within this function are not defined outside of it
-        updateText(resultConditions)
+        updateTextCond(resultConditions)
     });
 }
 
-function updateText(resultConditions){
-    var city = resultConditions.current_observation.display_location.city;
-    $("#title").text("Weather for " + city);
+
+function updateTextFore(resultForecast){
+    var shortFore = resultForecast.forecast.simpleforecast
+
+    //Date
+    $("#Day1").text(shortFore.forecastday[0].date.weekday + " - " + shortFore.forecastday[0].date.month + "/" + shortFore.forecastday[0].date.day + "/" + shortFore.forecastday[0].date.year);
+    $("#Day2").text(shortFore.forecastday[1].date.weekday + " - " + shortFore.forecastday[1].date.month + "/" + shortFore.forecastday[1].date.day + "/" + shortFore.forecastday[1].date.year);
+    $("#Day3").text(shortFore.forecastday[2].date.weekday + " - " + shortFore.forecastday[2].date.month + "/" + shortFore.forecastday[2].date.day + "/" + shortFore.forecastday[2].date.year);
+    $("#Day4").text(shortFore.forecastday[3].date.weekday + " - " + shortFore.forecastday[3].date.month + "/" + shortFore.forecastday[3].date.day + "/" + shortFore.forecastday[3].date.year);
+
+    //Conditions
+    $("#Day1Cond").text(shortFore.forecastday[0].conditions);
+    $("#Day2Cond").text(shortFore.forecastday[1].conditions);
+    $("#Day3Cond").text(shortFore.forecastday[2].conditions);
+    $("#Day4Cond").text(shortFore.forecastday[3].conditions);
+
+    //Icon
+    $("#Day1Icon").attr('src', shortFore.forecastday[0].icon_url);
+    $("#Day2Icon").attr('src', shortFore.forecastday[1].icon_url);
+    $("#Day3Icon").attr('src', shortFore.forecastday[2].icon_url);
+    $("#Day4Icon").attr('src', shortFore.forecastday[3].icon_url);
+
+
+    //High-Low
+    $("#Day1HighLow").text("High: " + shortFore.forecastday[0].high.fahrenheit + "°F Low: " + shortFore.forecastday[0].low.fahrenheit + "°F");
+    $("#Day2HighLow").text("High: " + shortFore.forecastday[1].high.fahrenheit + "°F Low: " + shortFore.forecastday[1].low.fahrenheit + "°F");
+    $("#Day3HighLow").text("High: " + shortFore.forecastday[2].high.fahrenheit + "°F Low: " + shortFore.forecastday[2].low.fahrenheit + "°F");
+    $("#Day4HighLow").text("High: " + shortFore.forecastday[3].high.fahrenheit + "°F Low: " + shortFore.forecastday[3].low.fahrenheit + "°F");
+
+    //Conditions
+
+
+}
+function updateTextCond(resultConditions){
+    $("#title").text("Weather for " + resultConditions.current_observation.display_location.full);
 }
